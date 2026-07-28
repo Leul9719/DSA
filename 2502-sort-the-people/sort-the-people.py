d88c80c9aa617da31e3450f59e9b0636e222d06d
Leul9719 <lmekonnen793@gmail.com>
2026-07-28 20:57:26 +0300

    Time: 598 ms (5.53%) | Memory: 19.6 MB (51.69%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,28 +1,60 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
 
+        """min_index = 0
+
+        n = len(names)
+        while min_index < n:
+            curr_max = min_index
+            for i in range(min_index,n):
+                if heights[i] > heights[curr_max]:
+                    curr_index = i
+            heights[min_index], heights[curr_max] = heights[curr_max],heights[min_index]
+
+            names[min_index], names[curr_max] = names[curr_max],names[min_index]
+
+            min_index += 1
+        return names"""
+
+        """n = len(names)
+
+        for i in range(1,n):
+            index = i
+
+            while index > 0 and heights[index] < heights[index - 1]:
+
+                heights[index],heights[index - 1] = heights[index - 1],heights[index]
+                names[index],names[index - 1] = names[index - 1],names[index]
+
+                index -= 1
+
+        return names"""
+
+        """n = len(names)
+
+        for i in range(1,n):
+            index = i
+
+            while index > 0 and heights[index] < heights[index - 1]:
+
+                heights[index],heights[index - 1] = heights[index - 1],heights[index]
+                names[index],names[index - 1] = names[index - 1],names[index]
+
+                index -= 1
+
+        return names[::-1]"""
+
         n = len(names)
 
-        #mapp = {heights[i]:names[i] for i in range(len(names))}
+        for i in range(1,n):
+            index = i
 
-        for i in range(n):
+            while index > 0 and heights[index] > heights[index - 1]:
 
-            for j in range(n - 1):
-                if heights[j] < heights[j + 1]:
-                    #heights[j],heights[j+1]=heights[j+1],heights[j]
-                    #swap the hight
-                    temp = heights[j]
-                    heights[j]=heights[j+1]
-                    heights[j+1]=temp
+                heights[index],heights[index - 1] = heights[index - 1],heights[index]
+                names[index],names[index - 1] = names[index - 1],names[index]
 
-                    #swap the names
+                index -= 1
 
-                    temp = names[j]
-                    names[j]=names[j+1]
-                    names[j+1]=temp
-
-
-        
-
-        #sorted_name=[mapp[heights[i]] for i in range(n)]
         return names
+
